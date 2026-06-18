492ebabb50575ca4ebdd0412c70888004ec1b011
ADONAI T WELDEMICAEL <78896535+adulegendary@users.noreply.github.com>
2026-06-18 13:58:30 -0700

    Add: sql-highest-grade-for-each-student - submission-0

---
+++ SQL/sql-highest-grade-for-each-student/submission-0.sql
@@ -0,0 +1,11 @@
+-- Write your query below
+SELECT student_id, exam_id, score
+FROM (
+    SELECT *,
+    ROW_NUMBER() OVER(
+         PARTITION BY student_id
+         ORDER BY score DESC, exam_id ASC
+    ) AS rn
+    FROM exam_results
+) t
+WHERE rn  = 1
