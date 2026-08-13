39d53816d96ccc6548b2df2327db69f88736be85
ADONAI T WELDEMICAEL <78896535+adulegendary@users.noreply.github.com>
2026-08-13 14:43:55 -0700

    Add: first-unique-number - submission-1

---
+++ Data Structures & Algorithms/first-unique-number/submission-1.java
@@ -0,0 +1,41 @@
+class FirstUnique {
+    Map<Integer, Integer> freq;
+    Queue<Integer> q ;
+
+    public FirstUnique(int[] nums) {
+        freq = new HashMap<>(); 
+        q = new LinkedList<>();
+        for(int i : nums){
+            freq.put(i, freq.getOrDefault(i, 0)+1);
+            q.add(i);
+        }
+    }
+    
+    public int showFirstUnique() {
+         System.out.println("first");
+         int [] arr = new int[3];
+         int index =0;
+         for(int val : q){
+             if(freq.get(val) ==1 ){
+                 return val;
+             }
+            
+            
+         }
+        
+
+         return -1;
+    }
+    
+    public void add(int value) {
+        freq.put(value, freq.getOrDefault(value, 0)+1);
+        q.add(value);
+    }
+}
+
+/**
+ * Your FirstUnique object will be instantiated and called as such:
+ * FirstUnique obj = new FirstUnique(nums);
+ * int param_1 = obj.showFirstUnique();
+ * obj.add(value);
+ */
