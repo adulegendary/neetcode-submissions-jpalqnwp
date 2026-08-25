c187100a51837174e04bad7c2b3b6670bc8a9198
ADONAI T WELDEMICAEL <78896535+adulegendary@users.noreply.github.com>
2026-08-25 09:40:46 -0700

    Add: minimum-remove-to-make-valid-parentheses - submission-2

---
+++ Data Structures & Algorithms/minimum-remove-to-make-valid-parentheses/submission-2.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        if(s.length() ==0) return s;
+
+        
+        Stack<Pair<Character, Integer>> stack = new Stack<>();
+      
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(!Character.isLetter(ch)){
+                 if(stack.isEmpty()){
+                     stack.push(new Pair<>(ch, i));
+                 }else if(ch == ')' && stack.peek().getKey() == '('){
+                     stack.pop();
+                 }else{
+                     stack.push(new Pair<>(ch, i));
+                 }
+            }
+        }
+        // [ ), ), (, (]
+       if(stack.isEmpty()) return s;
+         
+       StringBuilder result = new StringBuilder();
+       for(int i=s.length()-1; i>=0; i--){
+           char ch =  s.charAt(i);
+            if(Character.isLetter(ch)){
+                  result.append(ch);
+            }else {
+                  
+                if(!stack.isEmpty() && ch == stack.peek().getKey() && i == stack.peek().getValue()){
+                     stack.pop();
+                }else{
+                     result.append(ch);
+                }
+            }
+       }
+
+       return result.reverse().toString(); 
+       
+    }
+}
