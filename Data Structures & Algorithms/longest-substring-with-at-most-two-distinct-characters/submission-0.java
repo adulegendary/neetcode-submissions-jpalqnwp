4f8536e6a9552fd8c2716a68ef23c2ea23263e2e
ADONAI T WELDEMICAEL <78896535+adulegendary@users.noreply.github.com>
2026-08-27 19:48:07 -0700

    Add: longest-substring-with-at-most-two-distinct-characters - submission-0

---
+++ Data Structures & Algorithms/longest-substring-with-at-most-two-distinct-characters/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        
+
+        int left =0;
+       
+        int result = 0 ;
+
+        Map<Character, Integer> store = new HashMap<>();
+        for(int right =0; right < s.length(); right++){
+            char ch = s.charAt(right);
+            store.put(ch, store.getOrDefault(ch, 0)+1);
+            while(left < s.length() && store.size() > 2){
+               
+                  char ch2 = s.charAt(left);
+                  store.put(ch2, store.getOrDefault(ch2, 0)-1);
+                  if(store.getOrDefault(ch2, 0) == 0){
+                      store.remove(ch2);
+                  }
+                System.out.println(store + "  inside");
+                  left +=1;
+            }
+             System.out.println(store);
+            
+
+            result = Math.max(right-left+1, result);
+             System.out.println(result);
+        }
+
+        return result;
+    }
+}
